4d5437dbc973298b86e1693889b0c964dfb6bf12
Ivan Lazarov <ivan@betixon.com>
2022-09-22 20:26:32 +0300

    Created a download functionallity for the getItems and still could not finish the delete functionallity

--- controllers/actorController.js
+++ controllers/actorController.js
@@ -1,38 +1,52 @@
 const knex = require("../database/database");
 const fileSystem = require("fs");
 const fastcsv = require("fast-csv");
-const { resourceLimits } = require("worker_threads");
+const e = require("express");
 
-const getTableItems = async (req, res) => {
-    const [schema, table] = Object.values(req.params);
-    // const [schema, table, download] = Object.values(req.params);
-    // if(typeof download === bool){
-    //     res.send(`Download should be true or false`);
-    //     return;
-    // }
-    if(schema && table){
-        
-        const information = await knex.withSchema(schema)
-        .select('*')
-        .from(table);
-        
- 
-        // if(download){
-        //     const writeStream = fileSystem.createWriteStream(`./files/${table}-information.csv`);
-        //     fastcsv.write(information)
-        //         .on("finish", ()=>{
-        //             res.send(`<a href='./files/${table}-information.csv' download='${table}-information.csv' id='download-link'></a><script>document.getElementById('download-link').click();</script>`)
-        //         }).pipe(writeStream);
-        // }else{
+
+const getTableItems = async (req,res) =>{
+    let [schema, table, download] = Object.values(req.params);
+    if(download===undefined || download.toLowerCase() !== "true"){
+        if(schema && table ){
+            const information = await knex.withSchema(schema)
+            .select('*')
+            .from(table);
+
             res.send(information);
-        // }
-
+        }else{
+            res.sendStatus(404);
+        }
     }
-    else{
-        res.sendStatus(404);
-    }
+    else if(download.toLowerCase() ==="true"){
+        download=true;
+ 
+        if(download!==true){
+            res.send(`Download should be true`); 
+        }
+        else if(schema && table ){
+            const information = await knex.withSchema(schema)
+            .select('*')
+            .from(table);
+           
 
+ 
+            if(download){
+                const writeStream = fileSystem.createWriteStream(`./files/${table}-information.csv`);
+                fastcsv.write(information)
+                    .on("finish", ()=>{
+                        // res.send(`<a href='./files/${table}-information.csv' download='${table}-information.csv' id='download-link'></a><script>document.getElementById('download-link').click();</script>`)
+                        console.log("The file is about to be downloaded");
+                    }).pipe(writeStream);
+             
+                res.download(`./files/${table}-information.csv`);
+                }
+                else{
+                    res.sendStatus(404);
+                }
 
+        }
+    } 
+    
 }
 
 const getTableItem = async (req, res) => {
@@ -40,50 +54,34 @@ const getTableItem = async (req, res) => {
     const [schema, table, id] = Object.values(req.params);
     const requestedId =  parseInt(id);
 
-    console.log(schema, table, typeof requestedId);
-    if(requestedId === null || requestedId===undefined || isNaN(requestedId)){
-        res.sendStatus(404);
-    }else{  
-        const actor = await knex.withSchema(schema)
-        .select('*')
-        .from(table).where(`${table}_id`, requestedId);
-        if(actor){
-            res.send(actor);
-        }else{
+    if(schema && table){
+        if(requestedId === null || requestedId===undefined || isNaN(requestedId)){
             res.sendStatus(404);
+        }else{  
+            const actor = await knex.withSchema(schema)
+            .select('*')
+            .from(table).where(`${table}_id`, requestedId);
+            if(actor){
+                res.send(actor);
+            }else{
+                res.sendStatus(404);
+            }
+            
         }
-        
+    }else{
+        res.sendStatus(404);
     }
+    
     
 }
 
-// const getTableItemByParameters = async (req, res) => {
 
-//     const [schema, table, id, first_name, last_name] = Object.values(req.params);
-//     const requestedId =  parseInt(id);
-
-//     if(requestedId === null || requestedId===undefined || isNaN(requestedId)){
-//         res.sendStatus(404);
-//     }else{  
-//         const actor = await knex.withSchema(schema)
-//         .select('*')
-//         .from(table).where(`${table}_id`, requestedId).andWhere();
-//         if(actor){
-//             res.send(actor);
-//         }else{
-//             res.sendStatus(404);
-//         }
-        
-//     }
-    
-// }
 
 const postItem = async (req, res) => {
     
     const [schema, table] = Object.values(req.params);
     const currentDate = new Date();
     
-    console.log(req.body);
     if(schema && table){
         const idCount = await knex.withSchema(schema)
         .count(`${table}_id`)
@@ -146,29 +144,35 @@ const updateItemById = async (req, res) => {
 }
 
 const deleteItemById = async (req, res) => {
-    const [schema, table, requestedId] = Object.values(req.params);
+    const [schema, table, id] = Object.values(req.params);
+    const requestedId =  parseInt(id);
+
     console.log(schema, table, requestedId);
-    if(requestedId === null || requestedId===undefined || typeof requestedId !== typeof number){
-        res.sendStatus(404);
-        return;
-    }
-    if(table === "address"){
-        await knex.withSchema(schema)
-        .from(table).where(`${table}_id`, requestedId).using(table).whereRaw(`${table}.${table}_id = customer.${table}_id`).del();
+    if(schema && table){
+        if(requestedId === null || requestedId===undefined || isNaN(requestedId)){
+            res.sendStatus(404);
+            
+        }else{
+            await knex.withSchema(schema)
+            // .from(table).where(`${table}_id`, requestedId).del();
+            .where(knex.raw(`DELETE FROM ${table} WHERE ${table}_id=${requestedId}`));
+    
+        
+    
+            res.send(`${table}_id ${requestedId} has been deleated from table ${table}!`);
+        }
+        
+            
     }else{
-        await knex.withSchema(schema)
-        .from(table).where(`${table}_id`, requestedId).del();
+        res.sendStatus(404);  
     }
     
-
-    res.send(`${table}_id ${requestedId} has been deleated from table ${table}!`);
 }
 
 
 module.exports = {
     getTableItems,
     getTableItem,
-    // getTableItemByParameters,
     postItem,
     updateItemById,
     deleteItemById
--- routes/actorRouter.js
+++ routes/actorRouter.js
@@ -5,18 +5,16 @@ const router = express.Router();
 const {
     getTableItems,
     getTableItem,
-    // getTableItemByParameters,
+
     postItem,
     updateItemById,
     deleteItemById
 } = require("../controllers/actorController");
 
 router.get("/:schema-:table", getTableItems);
-// router.get("/:schema-:table/:download", getTableItems);
+router.get("/:schema-:table/download=:true", getTableItems);
 
 router.get("/:schema-:table/:id", getTableItem);
-
-// router.get("/:schema-:table/:id&:first_name&:last_name", getTableItemByParameters);
 
 router.post("/:schema-:table",postItem);
 
